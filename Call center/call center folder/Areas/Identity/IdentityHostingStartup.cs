c7db813b9c7260f735697855fa4654e1e50a62a9
praveena-12 <104411688+praveena-12@users.noreply.github.com>
2022-06-02 15:35:16 +0530

    Add files via upload

---
+++ Call center/call center folder/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using CallCenter.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(CallCenter.Areas.Identity.IdentityHostingStartup))]
+namespace CallCenter.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<CallCenterContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("CallCenterContextConnection")));
+
+                services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<CallCenterContext>();
+            });
+        }
+    }
+}
